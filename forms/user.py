e804091ae36a1e46424f0dbc9c8a7f780351870c
Leonardo <LeonardoChe888@ya.ru>
2025-04-30 21:18:54 +0700

    Создание обработчиков для регистрации и входа

---
+++ forms/user.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField, EmailField, ValidationError, BooleanField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField("Login / email", validators=[DataRequired()])
+    username = StringField("Surname", validators=[DataRequired()])
+    tg_name = StringField("Name", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password_again = PasswordField("Repeat password", validators=[DataRequired()])
+
+    def validate_tg_name(self, tg_name):
+        if '@' not in tg_name.data:
+            raise ValidationError('Telegram name must contain the "@" symbol.')
+
+    submit = SubmitField("Submit")
+
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember me")
+    submit = SubmitField("Enter")
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from data import db_session
+from data.users import User
+from forms.user import RegisterForm, LoginForm
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "produs_private_key"
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 def main():
@@ -10,9 +21,44 @@ def main():
     app.run()
 
 
+@app.route("/register", methods=["GET", "POST"])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template("register.html", title="Регистрация", form=form, massage="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template("register.html", title="Регистрация", form=form,
+                                   massage="Такой пользователь уже существует")
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            tg_name=form.tg_name.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Registrations', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template("login.html", massage="Incorrect login or password", form=form, title='Authorization')
+    return render_template('login.html', title='Authorization', form=form)
+
+
 @app.route("/")
 def index():
-    pass
+    return render_template("base.html")
 
 
 if __name__ == "__main__":
